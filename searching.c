9f7e9840d2b032b3bb018241de0b22e7fe06df9d
gurpartap <dhaliwal.gurpartap@gamil.com>
2018-07-08 14:40:51 +0530

    linear and binary search

--- searching.c
+++ searching.c
@@ -1,19 +1,54 @@
 #include<stdio.h>
 
+//linear search
 void linear_search(int input[],int a,int n)
 {
   int k=0;
   for(int i=0;i<n;i++)
     if(input[i]==a)
     {
-      printf("number is present");
+      printf("number is present\n");
       k=1;
     }
   if(k!=1)
-  printf("number is not present");
+  printf("number is not present\n");
 }
 
-//void binary_search(int input[])
+//binary search
+void binary_search(int input[],int a,int n)
+{
+  int k=0,first,last,mid;
+
+  //sorting
+  for(int i=0;i<n;i++)
+    for (int j=i+1;j<n;j++)
+      if(input[i]>input[j])
+      {
+        int temp=input[j];
+        input[j]=input[i];
+        input[i]=temp;
+      }
+
+  first=0;
+  last=n-1;
+
+  while(first<=last)
+  {
+    mid=(first+last)/2;
+    if(a<input[mid])
+      last=mid-1;
+    else if(a==input[mid])
+    {
+      printf("number is present\n");
+      k=1;
+      break;
+    }
+    else
+      first=mid+1;
+  }
+  if(k!=1)
+  printf("number is not present\n");
+ }
 
 int main()
 {
@@ -26,7 +61,7 @@ int main()
   for(int i=0;i<n;i++)
     scanf("%d",&input[i]);
 
-  printf("enter you want to search");
+  printf("enter you want to search\n");
   scanf("%d",&a);
 
   printf("select your option\n");
@@ -35,8 +70,14 @@ int main()
 
   switch(opt)
   {
-    case 1:linear_search(input,a,n);
-    //case 2:binary_search(input,a,n);
+    case 1:{
+              linear_search(input,a,n);
+              break;
+            }
+    case 2:{
+              binary_search(input,a,n);
+              break;
+            }
   }
   return 0;
 }
